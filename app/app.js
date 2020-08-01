0418bceae7f9f77b7b753840a38b35040824dab8
Maciej <>
2020-08-01 20:47:59 +0200

    app.js clean up

--- app/app.js
+++ app/app.js
@@ -8,41 +8,4 @@ export const weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']
 
 weatherApp.component('search', new SearchComponent());
 
-weatherApp.controller('SearchController', [
-  '$scope',
-  '$location',
-  'cityService',
-  function (
-    $scope,
-    $location,
-    cityService
-  ) {
-    return new SearchController(
-      $scope,
-      $location,
-      cityService
-    )
-  }
-]);
-
 weatherApp.component('forecast', new ForecastComponent());
-
-weatherApp.controller('ForecastController', [
-  '$scope',
-  '$routeParams',
-  'cityService',
-  'weatherService',
-  function (
-    $scope,
-    $routeParams,
-    cityService,
-    weatherService
-  ) {
-    return new ForecastController(
-      $scope,
-      $routeParams,
-      cityService,
-      weatherService
-    )
-  }
-]);
