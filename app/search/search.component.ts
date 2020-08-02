2776fbe366ed068fc941cd062802f4bb93beabf0
Maciej <>
2020-08-02 20:23:47 +0200

    delete app

--- app/search/search.component.ts
+++
@@ -1,9 +0,0 @@
-import { SearchController } from "./search.controller";
-
-export class SearchComponent implements ng.IComponentOptions {
-
-  controller = ['$location', 'cityService', SearchController];
-
-  templateUrl = './search/search.html';
-
-}
